6f7db1fcae7b4eecb85618ae0eaf51f0a19eb1c0
Shoaib Alam <shoaibalammandokhail00@gmail.com>
2025-07-30 08:51:28 +0000

    conversion of datatypes

---
+++ 01_Basic/02_conversionOperations.js
@@ -0,0 +1,33 @@
+let score = "shoaib"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueinnumber = Number(score);
+//console.log(typeof valueinnumber);
+//console.log( valueinnumber);
+
+
+//"33" => 33
+//"33abc" => NaN
+//"abc" => NaN
+//true => ; false => 0
+
+let isLoggedIn = "shoaib"
+
+let booleanLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanLoggedIn);
+
+
+// 1 => true
+// 0 => false
+// "" => false
+// "abc" => true
+// null => false
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+//console.log(stringNumber);
+
+console.log( typeof stringNumber); 
