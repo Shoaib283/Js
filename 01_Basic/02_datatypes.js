4fad4d257157481a9877d6270339c91622ad51f1
Shoaib Alam <shoaibalammandokhail00@gmail.com>
2025-07-30 08:27:13 +0000

    datatypes

---
+++ 01_Basic/02_datatypes.js
@@ -0,0 +1,27 @@
+//alert(3 + 3) we are using node js, not browser
+
+console.log(3 + 3)
+console.log("Hello World")
+
+
+let name = "shoaib "    
+let age = 20
+let isLoggedIn = false
+let state;
+//let state = null or "";
+
+// number => 2 to pwr 53
+// bigint => 2 to pwr 64
+//string => ""
+//boolean => true/false
+//null => empty value
+//undefined => variable not assigned
+// symbol => unique value (WE STUDY in react)
+
+
+//objects
+
+console.log(typeof "shoaib");
+console.log(typeof null); // types is object, but it is null
+console.log(typeof undefined);// type is undefined
+
