2fbf5b3ffecf9d7a8410e89e2564e6fe364fa313
Shoaib Alam <shoaibalammandokhail00@gmail.com>
2025-08-05 10:10:43 +0000

    datatype summary

---
+++ 01_Basic/datatypes_summary.js
@@ -0,0 +1,26 @@
+// primitive
+ // 7 types: string, number, boolean, null, undefined, symbol, bigint
+const score = 100
+const scorevalue = 100.3
+
+const isloggedin = false
+const outsidetemp = null
+let useremail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId );
+
+
+
+ // non-primitive (refrence)
+ //Array, objects, functions
+
+
+const heros =["shoaib", "doga", "shaktiman"];
+let myObj = {
+    name: "alam",
+    age: 22,
+}
+
